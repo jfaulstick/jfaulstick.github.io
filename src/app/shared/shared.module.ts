98f2f4c7c83e8949b36b7860ccc724ba1daf7fa4
Joe Faulstick <jfaulstick@gmail.com>
2020-07-22 14:58:51 -0400

    WIP new social button

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SocialButtonComponent } from './social-button/social-button.component';
+
+
+
+@NgModule({
+  declarations: [SocialButtonComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
